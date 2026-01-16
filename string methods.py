60de4bfbe8e93945c2d16870f2e85ecfe67fdfcd
janvipatil585-collab <janvipatil585@gmail.com>
2026-01-16 20:43:32 +0530

    string methods used in python
    STRING METHODS SUCH AS UPPER,LOWER,CAPITALIZE ARE USED TO MAKE OUR STRING INTO ALL LETTERS CAPITAL OR ALL LETTERS TO BE SMALL OR ONLY FIRST LETTER TO BE CAPITAL

---
+++ string methods.py
@@ -0,0 +1,10 @@
+str=input('enter your string')
+
+str.upper()
+print('the upper case string is:',str.upper())
+
+str.lower()
+print('the lower case string is:',str.lower())
+
+str.capitalize()
+print('the capitalized string is:',str.capitalize())
